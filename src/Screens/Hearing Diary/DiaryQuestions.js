0db6ad49ec45fae518ca2821574cce67b75fb18a
vijay.kumar@iorbit-tech.com <vijay.kumar@iorbit-tech.com>
2022-12-07 18:46:50 +0530

    Question options mapping updated

--- src/Screens/Hearing Diary/DiaryQuestions.js
+++ src/Screens/Hearing Diary/DiaryQuestions.js
@@ -8,15 +8,6 @@ import Radiobutton from '../../Components/Field/RadioButton';
 import SubmitButton from '../../Components/SubmitButton';
 import { NEXT } from '../../Constants/appconstants';
 
-const Questions = [
-    { value: 0, question: 1, name: "The Situation.", options: ["Restaurant", "Home", "Group conversation at work", "Outside"] },
-    { value: 1, question: 2, name: "The Problem.", options: ["Hearing aid ringing", "Could not understand female voices", "Overall difficulty following conversation"] },
-    { value: 2, question: 3, name: "Time of day.", options: ["Morning", "Afternoon", "Evening"] },
-    { value: 3, question: 4, name: "Hours hearing aid worn when difficulty occurred.", options: [{ label: '1 hour', value: 0 }, { label: '4 hours', value: 1 }, { label: '6 hours', value: 2 }, { label: '8 hours', value: 3 }, { label: 'Greater than 8 hours', value: 4 }], type: 'radio' },
-    { value: 4, question: 5, name: "How did you react to the situation?", options: ["I did nothing and listened the best I could", "I did nothing and listened the best I could", "I  adjusted my hearing aid setting", "I removed myself from the situation"] },
-    { value: 5, question: 6, name: "Did your reaction help?", options: [{ label: 'Yes', value: 0 }, { label: 'No', value: 1 }], type: 'radio' }
-];
-
 const DiaryQuestions = ({ navigation }) => {
     const [questionIndex, setQuestionIndex] = useState(0);
     const [value, setValue] = useState(0);
@@ -26,24 +17,17 @@ const DiaryQuestions = ({ navigation }) => {
     const submit = (value) => {
         console.log(value, 'value');
     }
-    console.log(questionIndex, 'questionIndex');
-    console.log(questions.length - 1, 'length');
-    // console.log(questions[questionIndex].answerType, 'questions')
+
     useEffect(() => {
         if (questionIndex < questions.length) {
-            setOptionsList([{
-                label: questions[questionIndex].options[0], value: 0
-            },
-            {
-                label: questions[questionIndex].options[1], value: 1
-            },
-            {
-                label: questions[questionIndex].options[2], value: 2
-            },
-            {
-                label: questions[questionIndex].options[3], value: 3
-            },
-            ])
+            let dataList = [];
+            dataList = questions[questionIndex].options.map((option, i) => (
+                {
+                    label: option, value: i
+                }
+            )
+            )
+            setOptionsList(dataList);
         }
     }, [questionIndex]);
 
--- src/Screens/TellUs/Survey.js
+++ src/Screens/TellUs/Survey.js
@@ -6,40 +6,23 @@ import RadioForm, { RadioButton, RadioButtonInput, RadioButtonLabel } from 'reac
 import { NEXT } from '../../Constants/appconstants';
 import { useSelector } from 'react-redux';
 
-const Questions = [
-    { id: 0, question: 1, name: "I have worn a hearing device?", },
-    { id: 1, question: 2, name: "How long do you have any discomfort using the Hearing Device?", },
-    { id: 2, question: 3, name: "How long are you using this device?", },
-    { id: 3, question: 4, name: "At what age you started using the device?", },
-    { id: 4, question: 5, name: "How long you are using this Brand of your Hearing Device?" },
-];
-
 const Survey = ({ navigation }) => {
     const [value, setValue] = useState(0);
     const [questionIndex, setQuestionIndex] = useState(0);
     const [optionsList, setOptionsList] = useState([]);
     const { questions } = useSelector((state) => state.tellus);
 
-    console.log(questionIndex)
     useEffect(() => {
-        // if (questionIndex < (questions.length - 1)) {
-        setOptionsList([{
-            label: questions[questionIndex].options[0], value: 0
-        },
-        {
-            label: questions[questionIndex].options[1], value: 1
-        },
-        {
-            label: questions[questionIndex].options[2], value: 2
-        },
-        {
-            label: questions[questionIndex].options[3], value: 3
-        },
-        ])
-        // }
+        let dataList = [];
+        dataList = questions[questionIndex].options.map((option, i) => (
+            {
+                label: option, value: i
+            }
+        )
+        )
+        setOptionsList(dataList);
     }, [questionIndex]);
-    console.log(questionIndex, 'questionIndex');
-    console.log(questions.length - 1, 'length');
+
     return (
         <View style={{ flex: 1, backgroundColor: '#fff' }}>
             <View style={{ padding: 20 }}>
@@ -50,10 +33,10 @@ const Survey = ({ navigation }) => {
                 <Text style={{ fontSize: 18, fontWeight: '600' }}>{questions[questionIndex] !== undefined && questions[questionIndex].question}</Text>
             </View>
             <View style={{ padding: 20, marginTop: 10 }}>
-                {/* {optionsList !== undefined && */}
                 <RadioForm>
                     {
                         optionsList.map((obj, i) => (
+                            console.log(obj, 'obj'),
                             <RadioButton labelHorizontal={true} key={i} >
                                 <RadioButtonInput
                                     obj={obj}
@@ -78,14 +61,12 @@ const Survey = ({ navigation }) => {
                         ))
                     }
                 </RadioForm>
-                {/* } */}
             </View>
             <View style={{ width: getWidth() / 1.2, alignSelf: 'center', marginTop: getHeight() / 1.3, position: 'absolute' }}>
                 <SubmitButton
                     text={NEXT}
                     btnStyle={{ backgroundColor: 'blue', padding: 20, borderRadius: 10 }}
                     textStyle={{ color: '#fff', fontWeight: 'bold', textAlign: 'center' }}
-                    // submit={() => (questions[questionIndex] !== undefined) ? setQuestionIndex(questionIndex + 1) : navigation.navigate('Dashboard')}
                     submit={() => (questionIndex < (questions.length - 1)) ? setQuestionIndex(questionIndex + 1) : navigation.navigate('Dashboard')}
 
                 />
