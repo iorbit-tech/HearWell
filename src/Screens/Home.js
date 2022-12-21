7e6603900a5fb93e005d38cbdb94bdf4aac03957
vijaykumariorbit <vijay.kumar@iorbit-tech.com>
2022-12-21 16:35:16 +0530

    Scoll issue fixed in HomePage

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { KeyboardAvoidingView, ScrollView, View } from 'react-native';
+import { Keyboard, KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
 import AppleAuth from './authentication/AppleAuth';
 import Form from './authentication/LoginForm';
 import GoogleAuth from './authentication/GoogleAuth';
@@ -7,11 +7,13 @@ import GoogleAuth from './authentication/GoogleAuth';
 const Home = () => {
 
     return (
+        <KeyboardAvoidingView behavior = {Platform.OS === 'ios'&& 'padding'} style = {{flex: 1}}>
         <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ flexGrow: 1, backgroundColor: '#fff', justifyContent: 'center' }}>
             <GoogleAuth />
             {/* <AppleAuth /> */}
             <Form />
         </ScrollView>
+        </KeyboardAvoidingView>
     )
 };
 
